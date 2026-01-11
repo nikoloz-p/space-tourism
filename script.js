f22038fc8ffe00201197575d902576c73c346101
Nika <pichkhaianikoloz@gmail.com>
2026-01-11 05:51:52 +0400

    landing page, mobile

---
+++ script.js
@@ -0,0 +1,12 @@
+// side nav menu
+const burgerMenu = document.getElementById('burger_menu');
+const headerNav = document.getElementById('header_nav_items');
+const closeNavBtn = document.getElementById('close_nav');
+
+burgerMenu.addEventListener('click', () => {
+    headerNav.classList.add('active');
+});
+
+closeNavBtn.addEventListener('click', () => {
+    headerNav.classList.remove('active');
+});
